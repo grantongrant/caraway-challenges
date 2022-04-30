c052b3de0d39743af9710c1a6dd2165299f0ca27
grantrussell <grant.ellis.russell@gmail.com>
2022-04-30 11:13:46 -0500

    challenge one complete

---
+++ src/components/ChallengeOne.js
@@ -0,0 +1,11 @@
+import './ChallengeOne.css';
+
+const ChallengeOne = () => {
+    return (
+        <div className="button-container">
+        <button id="add-to-cart">ADD TO CART</button>
+        </div>
+    )
+};
+
+export default ChallengeOne;
