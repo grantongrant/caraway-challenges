7aeaac439c3258e04359cb266c5ab5985b327f6d
grantrussell <grant.ellis.russell@gmail.com>
2022-05-01 22:58:38 -0500

    worked on challenge number 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import ChallengeOne from './components/ChallengeOne';
+import ChallengeOne from './components/ChallengeOne/ChallengeOne';
+import ChallengeTwo from './components/ChallengeTwo/ChallengeTwo';
 
 function App() {
   return (
     <div className="App">
       <ChallengeOne/>
+      <ChallengeTwo/>
     </div>
   );
 }
---
+++ src/components/ChallengeTwo/ChallengeTwo.js
@@ -0,0 +1,20 @@
+import "./ChallengeTwo.css";
+
+const ChallengeTwo = () => {
+    return (
+        <>
+        <div className="input-container">
+            <div className="wrapper">
+                <input className="input"type="email"></input>
+                <span className="label">Email Address</span>
+            </div>
+            <div className="wrapper">
+                <input className="input" type="password"/>
+                <span className="label">Password</span>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default ChallengeTwo;
