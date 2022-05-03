895653daa0e50ce075b695e8cb3640d04e2bd736
grantrussell <grant.ellis.russell@gmail.com>
2022-05-03 16:29:12 -0500

    worked on challenge two, the border eludes me

--- src/components/ChallengeTwo/ChallengeTwo.js
+++ src/components/ChallengeTwo/ChallengeTwo.js
@@ -6,11 +6,11 @@ const ChallengeTwo = ({challenges}) => {
         {challenges &&
         <div className="input-container">
             <div className="wrapper">
-                <input className="input"type="email"></input>
+                <input className="input email"type="email" placeholder=" "/>
                 <span className="label">Email Address</span>
             </div>
             <div className="wrapper">
-                <input className="input" type="password"/>
+                <input className="input password" type="password" placeholder=" "/>
                 <span className="label">Password</span>
             </div>
         </div>}
